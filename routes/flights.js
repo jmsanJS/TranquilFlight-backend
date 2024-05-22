6d8d612432d871ccaff0a4f5edd68108e0d31f8e
Mehdi <mehdi.karim43@gmail.com>
2024-05-22 10:06:33 +0200

    mise en place route backend API tracking vol recherche par numéro de vol

---
+++ routes/flights.js
@@ -0,0 +1,50 @@
+var express = require("express");
+var router = express.Router();
+
+//const User = require("../models/users");
+const { checkBody } = require("../modules/checkBody");
+
+const XRapidAPIKey = process.env.XRapidAPIKey;
+
+
+//Route du front pour contacter le back
+// fetch('http://localhost:3000/flights', {
+//     method:'GET',
+//     headers: { 'Content-Type': 'application/json' },
+// 	body: JSON.stringify({ flightNumber: flightNumber, date: formatedDate }),
+// }).then(response => response.json())
+// .then(data => {
+//     if (data.result) {
+
+//     }else if (data.result===false){
+
+//     }
+// });
+
+router.get("/", (req, res) => {
+
+    if (!checkBody(req.body, ["flightNumber", "date"])) {
+        res.json({
+        result: false,
+        error: "Tous les champs doivent être renseignés",
+        });
+        return;
+    }
+
+    fetch(`https://aerodatabox.p.rapidapi.com/flights/number/${req.body.flightNumber}/${req.body.date}`, {
+        method: 'GET',
+        headers: { 
+            'X-RapidAPI-Key': XRapidAPIKey,
+            'X-RapidAPI-Host': 'aerodatabox.p.rapidapi.com' },
+    }).then(response => response.json())
+    .then(data => {
+        console.log('test')
+        if (data) {
+            res.json({ result: true, flightData: data });
+        }else if (!data){
+            res.json({ result: false, message: 'Vol introuvable' });
+        }
+    });
+});
+
+module.exports = router;
